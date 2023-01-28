5ee4a34c6da30ada91f81511e86387fb5391a4ab
nakamuratakeru <nakamuratakeru@localhost>
2023-01-28 19:19:39 +0900

    修正しました

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -1,13 +1,12 @@
 package baseball;
 
 public class BaseBallTeam {
-    String name;
-    int win;
-    int lose;
-    int draw;
+    private String name;
+    private int win;
+    private int lose;
+    private int draw;
 
     public BaseBallTeam() {
-
     }
 
     public BaseBallTeam(String name, int win, int lose, int draw) {
@@ -16,48 +15,47 @@ public class BaseBallTeam {
         this.lose = lose;
         this.draw = draw;
 
-
     }
 
-    private String getName() {
+    public String getName() {
         return name;
     }
 
-    private void setName(String name) {
+    public void setName(String name) {
         this.name = name;
     }
 
-    private int getWin() {
+    public int getWin() {
         return win;
     }
 
-    private void setWin(int win) {
+    public void setWin(int win) {
         this.win = win;
     }
 
-    private int getLose() {
+    public int getLose() {
         return lose;
     }
 
-    private void setLose(int lose) {
+    public void setLose(int lose) {
         this.lose = lose;
     }
 
-    private int getDraw() {
+    public int getDraw() {
         return draw;
     }
 
-    private void setDraw(int draw) {
+    public void setDraw(int draw) {
         this.draw = draw;
     }
 
-    public static double getRate(double win, double lose) {
-        double result = win / (win + lose);
+    public double getRate() {
+        double result = (double)this.win / ((double)this.win + (double)this.lose);
         return result;
     }
 
     public void report() {
-        double rate = getRate((double)this.win, (double)this.lose);
+        double rate = getRate();
 
         System.out.println(this.name + "の2022年の成績は" + this.win + "勝" + this.lose + "敗" + this.draw + "分、勝率は" + rate + "です。");
     }
